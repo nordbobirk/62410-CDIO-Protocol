8510f55c4186efe59ccb9afd3cb1f766db6528d9
nordbobirk <36029360+nordbobirk@users.noreply.github.com>
2026-03-20 13:22:39 +0100

    parsing tests

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -6,7 +6,8 @@ import unittest
 from protocol import (
     CommandName, Instruction, Arguments, InstructionType,
     serialize_message, parse_message, serialize_arguments,
-    Message, Acknowledgement, serialize_ack
+    Message, Acknowledgement, serialize_ack, parse_ack,
+    parse_arguments
 )
 
 class TestPENISProtocol(unittest.TestCase):
@@ -61,6 +62,82 @@ class TestPENISProtocol(unittest.TestCase):
 
     # region parsing
 
+    def test_parse_arguments(self):
+        serialized = ";20;20;20;5.0;10;1;0;True;False;"
+        expected = Arguments()
+        arguments = parse_arguments(serialized.split(";"))
+        self.assertEqual(arguments.inst_id, expected.inst_id)
+        self.assertEqual(arguments.rspeed, expected.rspeed)
+        self.assertEqual(arguments.lspeed, expected.lspeed)
+        self.assertEqual(arguments.speed, expected.speed)
+        self.assertEqual(arguments.rotations, expected.rotations)
+        self.assertEqual(arguments.position, expected.position)
+        self.assertEqual(arguments.seconds, expected.seconds)
+        self.assertEqual(arguments.target_angle, expected.target_angle)
+        self.assertEqual(arguments.brake, expected.brake)
+        self.assertEqual(arguments.block, expected.block)
+        self.assertEqual(arguments.talk, expected.talk)
+
+        custom_arguments = Arguments(
+            inst_id="inst_id",
+            rspeed=0,
+            lspeed=0,
+            speed=0,
+            rotations=0.0,
+            position=0,
+            seconds=0,
+            target_angle=360,
+            brake=False,
+            block=True,
+            talk="yeet",
+        )
+        custom_serialized = serialize_arguments(custom_arguments)
+        self.assertEqual(custom_arguments.rspeed, 0)
+        self.assertEqual(custom_serialized, "inst_id;0;0;0;0.0;0;0;360;False;True;yeet")
+    
+    def test_parse_message(self):
+        serialized_message = "c_fwd:;20;20;20;5.0;10;1;0;True;False;\n"
+        message = parse_message(serialized_message)
+        instruction = message.instruction
+        arguments = instruction.args
+        self.assertEqual(instruction.name, CommandName.FORWARD.value)
+        self.assertEqual(instruction.type, InstructionType.COMMAND.value)
+        self.assertEqual(arguments.rspeed, 20)
+        self.assertEqual(arguments.lspeed, 20)
+        self.assertEqual(arguments.speed, 20)
+        self.assertEqual(arguments.rotations, 5.0)
+        self.assertEqual(arguments.position, 10)
+        self.assertEqual(arguments.seconds, 1)
+        self.assertEqual(arguments.target_angle, 0)
+        self.assertEqual(arguments.brake, True)
+        self.assertEqual(arguments.block, False)
+        self.assertEqual(arguments.talk, "")
+    
+    def test_parse_acknowledgement(self):
+        serialized_ack = "ACK {\"key\": \"value\"}\n"
+        ack = parse_ack(serialized_ack)
+        expected = Acknowledgement("ACK", { "key": "value" })
+        self.assertEqual(ack.status, expected.status)
+        self.assertEqual(ack.data, expected.data)
+
+        serialized_nak = "NAK {\"key\": \"value\"}\n"
+        nak = parse_ack(serialized_nak)
+        expected = Acknowledgement("NAK", { "key": "value"} )
+        self.assertEqual(nak.status, expected.status)
+        self.assertEqual(nak.data, expected.data)
+
+        serialized_ack_no_data = "ACK {}\n"
+        ack_no_data = parse_ack(serialized_ack_no_data)
+        expected = Acknowledgement("ACK")
+        self.assertEqual(ack_no_data.status, expected.status)
+        self.assertEqual(ack_no_data.data, expected.data)
+
+        serialized_nak_no_data = "NAK {}\n"
+        nak_no_data = parse_ack(serialized_nak_no_data)
+        expected = Acknowledgement("NAK")
+        self.assertEqual(nak_no_data.status, expected.status)
+        self.assertEqual(nak_no_data.data, expected.data)
+
     # endregion parsing
 
     # region roundtrip
@@ -68,9 +145,9 @@ class TestPENISProtocol(unittest.TestCase):
     def test_roundtrip_forward(self):
         """c_fwd serializes → parses back identically"""
         instruction = Instruction(
-            name=CommandName.FORWARD,
-            type=InstructionType.COMMAND,
-            args=Arguments(speed=50, seconds=2)
+            name = CommandName.FORWARD,
+            type = InstructionType.COMMAND,
+            args = Arguments(speed=50, seconds=2)
         )
         wire = serialize_message(message = Message(instruction = instruction))
         roundtrip = parse_message(wire)
